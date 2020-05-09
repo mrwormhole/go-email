13ec70b123d8ebdc9ad74e5f37dce6aec06d551f
mrwormhole <talhaaltinel@hotmail.com>
2020-05-09 04:35:49 +0100

    gorm features

--- entities/email.go
+++ entities/email.go
@@ -1,7 +1,22 @@
 package entity
 
+import (
+	"time"
+
+	"github.com/jinzhu/gorm"
+)
+
+type Person struct {
+	Name string `json:"name"`
+	gorm.Model
+}
+
 type Email struct {
-	Sender   string `json:"sender"`
-	Receiver string `json:"receiver"`
-	Message  string `json:"message"`
+	ID        uint64    `gorm:"primary_key;auto_increment" json:"id"`
+	Sender    Person    `json:"sender"`
+	Receiver  Person    `json:"receiver"`
+	Message   string    `json:"message"`
+	CreatedAt time.Time `json:"-" gorm:"default:CURRENT_TIMESTAMP" json:"created_at"`
+	UpdatedAt time.Time `json:"-" gorm:"default:CURRENT_TIMESTAMP" json:"updated_at"`
+	gorm.Model
 }
